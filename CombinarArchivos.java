8b6ee8b807fa0e6a39da9e8607804b0bbee93e26
dcolmenares <dannycolmenarespdv@gmail.com>
2016-09-16 14:38:21 -0400

    Update CombinarArchivos.java

--- CombinarArchivos.java
+++ CombinarArchivos.java
@@ -1,2 +1,11 @@
 /* Clase en java SE para combinar 2 o mas archivos en formato plano en un unico archivo.
+	1 Editar un archivo eliminando su encabezado y guardarlo en otro archivo con otro nombre indicado
+	2 Combinar 2 archivos eliminandoles el encabezado, dentro de un tercer archivo indicado
+	1 Tipo de combinacion de archivos:
+    	a Con encabezado
+    	b Sin encabezado
+	2 Listado de archivos a combinar
+	3 Nombre de archivo destino
+		1 validar que el numero de parametros que se pasan a la clase sea el suficiente para una ejecucion exitosa
+	2 Validar que efectivamente existan los archivos que se van a combinar en el metodo correspondiente de la clase
 */
