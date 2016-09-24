02b25c69f237b10cdeeac0f680df17b51256a23e
dcolmenares <dannycolmenarespdv@gmail.com>
2016-09-24 11:53:19 -0400

    Update CombinarArchivos.java

--- CombinarArchivos.java
+++ CombinarArchivos.java
@@ -13,20 +13,29 @@
 import java.io.*;
 
 public class CombinarArchivos{
-        
+
         public static void main(String[] args){
                 File archivo = null;
                 FileReader fr = null;
                 BufferedReader br = null;
-                System.out.println("\"Esta es una prueba de java\"");
+                System.out.println("El sistema operativo es " + System.getProperty("os.name"));
+                System.out.println("El simbolo de separacion es " + System.getProperty("file.separator"));
+                System.out.println("Se ingresaron " + args.length + " argumentos");
                 try{
                         System.out.println("Tratando de leer el archivo");
-                        archivo = new File("C:/archivo.txt");
-                        System.out.println("El archivo leido es " + archivo.getName());
+                        if (System.getProperty("os.name").equals("Linux")){
+                                archivo = new File("/home/dcolmenares/archivo.txt");
+                        }
+                        else{
+                                archivo = new File("C:/archivo.txt");
+                        }
+                        if (archivo.exists()){
+                                System.out.println("El archivo existe");
+                                System.out.println("El archivo leido es " + archivo.getName());
+                        }
                 }catch(Exception e){
                         System.out.println("Problemas al leer el archivo");
                 }finally{
-
                 }
         }
 }
